3c6ef26fe5d21e258acb2fcbff8e84ca05f5c087
minhvb <minhvb2310@gmail.com>
2020-02-04 12:48:18 +0700

    Test slack notification

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,6 +63,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        echo 'test';
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
