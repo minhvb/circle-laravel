b122fe6d1ebdf43637725c1f3a545b09a28822fe
minhvb <minhvb2310@gmail.com>
2020-02-04 12:53:30 +0700

    Test pipeline with PHPUnit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -61,8 +61,8 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \App\User
      */
-    protected function create
-    (array $data) {
+    protected function create(array $data)
+    {
         echo 'test';
         return User::create([
             'name' => $data['name'],
