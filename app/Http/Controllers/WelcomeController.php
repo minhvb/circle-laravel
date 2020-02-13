7638c6a43c00fc4dec3fe17cc20b8212472ea866
minhvb <minhvb2310@gmail.com>
2020-02-13 18:27:28 +0700

    Pbar 7 test 2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,8 +6,7 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view('welcome');
     }
 }
