a0f7af743ecc0a9ff1c699297e39ea0645f27954
minhvb <minhvb2310@gmail.com>
2020-02-13 18:22:24 +0700

    Pbar 7 test 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Circle Laravel Demo 1
                 </div>
 
                 <div class="links">
