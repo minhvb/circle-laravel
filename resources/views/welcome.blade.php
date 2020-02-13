0aca7e243f728dd84de28c51b6497d5684e3e0c4
minhvb <minhvb2310@gmail.com>
2020-02-13 16:54:56 +0700

    Test 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CircleCI Laravel Demo
+                    Laravel
                 </div>
 
                 <div class="links">
